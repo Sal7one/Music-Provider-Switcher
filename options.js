25134903206a537539b339855ed8a728645cd87f
Sal7one <Alanazi.saleh.77@gmail.com>
2022-01-18 22:41:55 +0300

    more speration of utils and other scopes

--- options.js
+++ options.js
@@ -34,14 +34,3 @@ Get().then(values=>{
       })
     
 
-function SetChrome(key, thingy) {
-    chrome.storage.local.set({ [key]: thingy });
-  }
-
-  function Get() {
-    return new Promise(function (resolve, _reject) {
-      chrome.storage.local.get(null, function (items) {
-        resolve(items);
-      });
-    });
-  }
