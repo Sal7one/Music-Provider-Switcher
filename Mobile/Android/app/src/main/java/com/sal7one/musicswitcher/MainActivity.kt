2ea3b01abdd482a8f82b9286ca39e0097acf5203
Sal7one <Alanazi.saleh.77@gmail.com>
2022-01-21 09:15:23 +0300

    mobile app started

---
+++ Mobile/Android/app/src/main/java/com/sal7one/musicswitcher/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.sal7one.musicswitcher
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
