f626e50dea3bf92ac52e5f7833536f086ef0fae8
Sal7one <Alanazi.saleh.77@gmail.com>
2022-01-29 20:10:04 +0300

    choosen button indicator

--- Android/app/src/main/java/com/sal7one/musicswitcher/MainActivity.kt
+++ Android/app/src/main/java/com/sal7one/musicswitcher/MainActivity.kt
@@ -2,10 +2,41 @@ package com.sal7one.musicswitcher
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageButton
+import com.sal7one.musicswitcher.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var spotify_btn: ImageButton
+    private lateinit var apple_btn: ImageButton
+    private lateinit var anghami_btn: ImageButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        spotify_btn =  binding.spotifybtn
+        apple_btn = binding.applemusicbtn
+        anghami_btn =  binding.anghamibtn
+
+
+        spotify_btn.setOnClickListener{changeViewBackground(it)}
+        apple_btn.setOnClickListener{changeViewBackground(it)}
+        anghami_btn.setOnClickListener{changeViewBackground(it)}
+    }
+
+
+    fun changeViewBackground(the_button: View){
+
+        spotify_btn.setBackgroundColor(getColor(R.color.white))
+        apple_btn.setBackgroundColor(getColor(R.color.white))
+        anghami_btn.setBackgroundColor(getColor(R.color.white))
+
+        the_button.setBackgroundColor(getColor(R.color.button_clicked))
     }
 }
