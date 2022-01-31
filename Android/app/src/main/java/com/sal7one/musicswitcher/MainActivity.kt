b624d944bfa92f88631a64385e85ac2a5a2d828d
Sal7one <Alanazi.saleh.77@gmail.com>
2022-01-31 06:31:38 +0300

    implementtion without MVVM first

--- Android/app/src/main/java/com/sal7one/musicswitcher/MainActivity.kt
+++ Android/app/src/main/java/com/sal7one/musicswitcher/MainActivity.kt
@@ -2,24 +2,30 @@ package com.sal7one.musicswitcher
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
+import androidx.lifecycle.ViewModelProvider
 import com.sal7one.musicswitcher.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    //private lateinit var viewModel: MainActicityViewModel
     private lateinit var binding: ActivityMainBinding
-
     private lateinit var spotify_btn: ImageButton
     private lateinit var apple_btn: ImageButton
     private lateinit var anghami_btn: ImageButton
     private lateinit var update_btn: Button
 
+    private var currentProvider = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+//        viewModel = ViewModelProvider(this).get(MainActicityViewModel::class.java)
 
         spotify_btn =  binding.spotifybtn
         apple_btn = binding.applemusicbtn
@@ -27,19 +33,36 @@ class MainActivity : AppCompatActivity() {
         update_btn =  binding.updatebutton
 
 
-        spotify_btn.setOnClickListener{changeViewBackground(it) }
-        apple_btn.setOnClickListener{changeViewBackground(it)}
-        anghami_btn.setOnClickListener{changeViewBackground(it)}
-        update_btn.setOnClickListener {}
+        spotify_btn.setOnClickListener{
+            changeViewBackground(it)
+            currentProvider = "spotify"
+        }
+        apple_btn.setOnClickListener{
+            changeViewBackground(it)
+            currentProvider = "applemusic"
+        }
+        anghami_btn.setOnClickListener{
+            changeViewBackground(it)
+            currentProvider = "anghami"
+        }
+        update_btn.setOnClickListener {
+            if(currentProvider.isNotBlank()){
+              updateProvider()
+            }
+        }
     }
 
 
     fun changeViewBackground(the_button: View){
-
         spotify_btn.setBackgroundColor(getColor(R.color.white))
         apple_btn.setBackgroundColor(getColor(R.color.white))
         anghami_btn.setBackgroundColor(getColor(R.color.white))
-
         the_button.setBackgroundColor(getColor(R.color.button_clicked))
+    }
+
+    fun updateProvider(){
+
+        //currentProvider
+
     }
 }
