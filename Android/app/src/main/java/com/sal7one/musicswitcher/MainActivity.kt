5feb0ddc6ba0bca8c8ae2f5de4dbc033acf7ee61
Sal7one <Alanazi.saleh.77@gmail.com>
2022-02-23 12:48:27 +0300

    changed click listener appraoch to xml

--- Android/app/src/main/java/com/sal7one/musicswitcher/MainActivity.kt
+++ Android/app/src/main/java/com/sal7one/musicswitcher/MainActivity.kt
@@ -5,33 +5,24 @@ import com.sal7one.musicswitcher.repository.DataStoreProvider
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
-import android.widget.ImageButton
+import android.view.View
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import com.sal7one.musicswitcher.databinding.ActivityMainBinding
 import com.sal7one.musicswitcher.controllers.MyViewModelFactory
 import com.sal7one.musicswitcher.controllers.ApplicationViewModel
 import com.sal7one.musicswitcher.utils.Constants
-import android.content.DialogInterface
-
-import android.text.Html
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: ApplicationViewModel
     private lateinit var binding: ActivityMainBinding
-    private lateinit var spotifyBtn: ImageButton
-    private lateinit var appleBtn: ImageButton
-    private lateinit var anghamiBtn: ImageButton
-    private lateinit var deezerBtn: ImageButton
-    private lateinit var ytBtn: ImageButton
-    private lateinit var updateBtn: Button
     private lateinit var dataStoreProvider: DataStoreProvider
     private var currentProvider = ""
-    private var playlistChoice = false
+    private var clickedMusicProvider = ""
     private var albumChoice = false
+    private var playlistChoice = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,57 +30,29 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         dataStoreProvider = DataStoreProvider(applicationContext).getInstance()
-
         viewModel = ViewModelProvider(this, MyViewModelFactory(dataStoreProvider)).get(
             ApplicationViewModel::class.java
         )
 
-        spotifyBtn = binding.spotifyBtn
-        appleBtn = binding.appleMusicBtn
-        anghamiBtn = binding.anghamiBtn
-        deezerBtn = binding.deezerBtn
-        ytBtn = binding.ytMusicBtn
-        updateBtn = binding.updateBtn
-
-        viewModel.chosenProvider.observe(this, {
+        viewModel.chosenProvider.observe(this) {
             currentProvider = it
             changeViewBackground()
-        })
+        }
 
-        viewModel.playlistChoice.observe(this, {
+        viewModel.playlistChoice.observe(this) {
             playlistChoice = it
-        })
+        }
 
-        viewModel.albumChoice.observe(this, {
+        viewModel.albumChoice.observe(this) {
             albumChoice = it
             changeCheckboxState()
-        })
+        }
 
         binding.explainButton.setOnClickListener {
             alertDialogMaker()
         }
 
-        spotifyBtn.setOnClickListener {
-            currentProvider = Constants.SPOTIFY.link
-            changeViewBackground()
-        }
-        appleBtn.setOnClickListener {
-            currentProvider = Constants.APPLE_MUSIC.link
-            changeViewBackground()
-        }
-        anghamiBtn.setOnClickListener {
-            currentProvider = Constants.ANGHAMI.link
-            changeViewBackground()
-        }
-        deezerBtn.setOnClickListener {
-            currentProvider = Constants.DEEZER.link
-            changeViewBackground()
-        }
-        ytBtn.setOnClickListener {
-            currentProvider = Constants.YT_MUSIC.link
-            changeViewBackground()
-        }
-        updateBtn.setOnClickListener {
+        binding.updateBtn.setOnClickListener {
             if (currentProvider.isNotBlank()) {
                 updateProvider()
             } else {
@@ -102,13 +65,12 @@ class MainActivity : AppCompatActivity() {
         }
         binding.playlistCheckBox.setOnClickListener {
             playlistChoice = binding.playlistCheckBox.isChecked
-            Log.d("TAGMANINE", "I will send  $playlistChoice")
         }
         binding.albumCheckBox.setOnClickListener {
             albumChoice = binding.albumCheckBox.isChecked
-            Log.d("TAGMANINE", "I will send  $albumChoice")
         }
     }
+
 
     private fun changeCheckboxState() {
         binding.playlistCheckBox.isChecked = playlistChoice
@@ -141,9 +103,20 @@ class MainActivity : AppCompatActivity() {
         AlertDialog.Builder(this)
             .setTitle("What to do")
             .setView(R.layout.explain_page)
-            .setPositiveButton("Done", DialogInterface.OnClickListener { dialog, _ ->
+            .setPositiveButton("Done") { dialog, _ ->
                 dialog.dismiss()
-            })
-            .show()
+            }.show()
+    }
+
+    fun buildClickListeners(musicButton: View) {
+        clickedMusicProvider = resources.getResourceEntryName(musicButton.id);
+            when (musicButton.id) {
+                R.id.spotifyBtn -> currentProvider = Constants.SPOTIFY.link
+                R.id.appleMusicBtn -> currentProvider = Constants.APPLE_MUSIC.link
+                R.id.anghamiBtn -> currentProvider = Constants.ANGHAMI.link
+                R.id.deezerBtn -> currentProvider = Constants.DEEZER.link
+                R.id.ytMusicBtn -> currentProvider = Constants.YT_MUSIC.link
+            }
+            changeViewBackground()
     }
 }
