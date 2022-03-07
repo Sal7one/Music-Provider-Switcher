71a30be914d4e8d69fb1278c42fe6e2f38b8b6d0
Sal7one <Alanazi.saleh.77@gmail.com>
2022-03-07 14:50:28 +0300

    re did ui in compose almost finished

--- Android/app/src/main/java/com/sal7one/musicswitcher/compose/ui/theme/Color.kt
+++ Android/app/src/main/java/com/sal7one/musicswitcher/compose/ui/theme/Color.kt
@@ -2,7 +2,24 @@ package com.sal7one.musicswitcher.compose.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+val White = Color(0xFFFFFFFF)
+
 val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+
+val AppPrimary_color = Color(0xFF240141)
+val primary_gradient_color = Color(0xFF0e0017)
+val TextSecondary_color = Color(0xFFC4A6D8)
+val Card_color = Color(0xFF37175E)
+val CardGlow_color = Color(0xFF9178BC)
+val BtnUpdate_color = Color(0xFF19132b)
+val apple_clicked_color = Color(0xFFF44336)
+val button_clicked_color = Color(0xFFDBDBDB)
+val spotify_clicked_color = Color(0xFF0DD507)
+val anghami_clicked_color = Color(0xFFA10CB3)
+val ytMusic_clicked_color = Color(0xFFFF0000)
+val deezer_clicked_color = Color(0xFF1BD3E4)
+val card_explain_color = Color(0xFF5b5080)
--- Android/app/src/main/java/com/sal7one/musicswitcher/compose/ui/theme/Type.kt
+++ Android/app/src/main/java/com/sal7one/musicswitcher/compose/ui/theme/Type.kt
@@ -2,27 +2,30 @@ package com.sal7one.musicswitcher.compose.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.sal7one.musicswitcher.R
 
 // Set of Material typography styles to start with
+val appFont = FontFamily(
+    Font(R.font.play_fair_regular, FontWeight.Normal)
+)
 val Typography = Typography(
     body1 = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = appFont,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
-    )
-    /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
     ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
+    h2 = TextStyle(
+        fontFamily = appFont,
         fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
+        fontSize = 19.sp
+    ),
+    button = TextStyle(
+        fontFamily = appFont,
+        fontWeight = FontWeight.W500,
+        fontSize = 15.sp
     )
-    */
 )
