dbd45a687340706f50b41ff3f1ffa80cbea57411
Sal7one <Alanazi.saleh.77@gmail.com>
2022-04-19 11:17:07 +0300

    progress indicator

--- Android/app/src/main/java/com/sal7one/musicswitcher/DeepLinkHandlerActivity.kt
+++ Android/app/src/main/java/com/sal7one/musicswitcher/DeepLinkHandlerActivity.kt
@@ -9,18 +9,28 @@ import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Scaffold
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModelProvider
 import com.android.volley.Request
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import com.sal7one.musicswitcher.compose.ui.theme.MusicSwitcherTheme
 import com.sal7one.musicswitcher.repository.DataStoreProvider
 import com.sal7one.musicswitcher.utils.MusicHelpers
 import com.sal7one.musicswitcher.viewmodels.DeepLinkHandlerViewModel
 import com.sal7one.musicswitcher.viewmodels.DeepLinkHandlerViewModelFactory
 
 
-class DeepLinkHandlerActivity : AppCompatActivity() {
+class DeepLinkHandlerActivity : ComponentActivity() {
 
     private lateinit var viewModel: DeepLinkHandlerViewModel
     private lateinit var data: Uri
@@ -30,50 +40,69 @@ class DeepLinkHandlerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        dataStoreProvider = DataStoreProvider(applicationContext).getInstance()
-        viewModel = ViewModelProvider(
-            this,
-            DeepLinkHandlerViewModelFactory(dataStoreProvider)
-        )[DeepLinkHandlerViewModel::class.java]
+        setContent {
+            MusicSwitcherTheme {
+                Scaffold(
+                    modifier = Modifier
+                        .width(50.dp)
+                        .height(50.dp)
+                        .background(Color.Transparent)
+                ) {
 
-        data = intent?.data!!
-        intent?.action.also { action = it }
+                    CircularProgressIndicator(
+                        modifier = Modifier
+                            .width(50.dp)
+                            .height(50.dp)
+                            .background(Color.Transparent)
+                    )
 
-        // data comes from datastore
-        viewModel.chosenProvider.observe(this) {
-            viewModel.handleDeepLink(data) // start opening app
-        }
+                    dataStoreProvider = DataStoreProvider(applicationContext).getInstance()
+                    viewModel = ViewModelProvider(
+                        this,
+                        DeepLinkHandlerViewModelFactory(dataStoreProvider)
+                    )[DeepLinkHandlerViewModel::class.java]
 
-        // Instantiate the RequestQueue.
-        viewModel.sameApp.observe(this) {
-            if (it) {
-                val appPackage = MusicHelpers.getMusicAppPackage(data.toString())
-                // Same provider - sameApp
-                openApp(action, data, appPackage)
-            }
-        }
+                    data = intent?.data!!
+                    intent?.action.also { action = it }
 
-        viewModel.differentApp.observe(this) {
-            val queue = Volley.newRequestQueue(this)
-
-            if (it) {
-                val songURL = data.toString()
-                val stringRequest = StringRequest(Request.Method.GET, songURL,
-
-                    { response ->
-                        val songName = MusicHelpers.extractFromURL(songURL, response)
-                        val searchURL = viewModel.searchLink.value
-                        val query: String = Uri.encode(songName, "utf-8")
-
-                        // Different Provider - differentApp
-                        val uri = Uri.parse(searchURL + query)
-                        openApp(Intent.ACTION_VIEW, uri, viewModel.musicPackage.value!!)
-                    },
-                    {
-                        Log.e("DEEP_LINK", "Volley Error")
+                    // data comes from datastore
+                    viewModel.chosenProvider.observe(this) {
+                        viewModel.handleDeepLink(data) // start opening app
                     }
-                )
-                queue.add(stringRequest)
+
+                    // Instantiate the RequestQueue.
+                    viewModel.sameApp.observe(this) {
+                        if (it) {
+                            val appPackage = MusicHelpers.getMusicAppPackage(data.toString())
+                            // Same provider - sameApp
+                            openApp(action, data, appPackage)
+                        }
+                    }
+
+                    viewModel.differentApp.observe(this) {
+                        val queue = Volley.newRequestQueue(this)
+
+                        if (it) {
+                            val songURL = data.toString()
+                            val stringRequest = StringRequest(Request.Method.GET, songURL,
+
+                                { response ->
+                                    val songName = MusicHelpers.extractFromURL(songURL, response)
+                                    val searchURL = viewModel.searchLink.value
+                                    val query: String = Uri.encode(songName, "utf-8")
+
+                                    // Different Provider - differentApp
+                                    val uri = Uri.parse(searchURL + query)
+                                    openApp(Intent.ACTION_VIEW, uri, viewModel.musicPackage.value!!)
+                                },
+                                {
+                                    Log.e("DEEP_LINK", "Volley Error")
+                                }
+                            )
+                            queue.add(stringRequest)
+                        }
+                    }
+                }
             }
         }
     }
