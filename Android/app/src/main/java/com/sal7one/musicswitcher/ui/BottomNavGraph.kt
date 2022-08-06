459dc99b78686ea65284681e64e55858f04883f2
Sal7one <Alanazi.saleh.77@gmail.com>
2022-08-06 23:10:11 +0300

    refactor

--- Android/app/src/main/java/com/sal7one/musicswitcher/ui/BottomNavGraph.kt
+++ Android/app/src/main/java/com/sal7one/musicswitcher/ui/BottomNavGraph.kt
@@ -1,6 +1,5 @@
 package com.sal7one.musicswitcher.ui
 
-import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -9,7 +8,6 @@ import com.sal7one.musicswitcher.ui.features.about.AboutScreen
 import com.sal7one.musicswitcher.ui.features.chooseMusicProvider.ChooseMusicProviderScreen
 import com.sal7one.musicswitcher.ui.features.options.OptionScreen
 
-@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun BottomNavGraph(navController: NavHostController) {
     NavHost(navController = navController, startDestination = "home") {
