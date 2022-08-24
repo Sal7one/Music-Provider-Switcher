5508ad384bcc3661680770955af742652cbd5dd4
Sal7one <Alanazi.saleh.77@gmail.com>
2022-08-24 16:26:48 +0300

    refactor

--- Android/app/src/main/java/com/sal7one/musicswitcher/domain/MusicProivders.kt
+++ Android/app/src/main/java/com/sal7one/musicswitcher/domain/MusicProivders.kt
@@ -7,7 +7,7 @@ import com.sal7one.musicswitcher.utils.StringConstants
 
 val musicProviders = listOf(
     MusicProvider(
-        "Apple Music",
+        StringConstants.APPLE_MUSIC_NAME.value,
         StringConstants.APPLE_MUSIC.value,
         StringConstants.APPLE_MUSIC_SEARCH.value,
         StringConstants.APPLE_MUSIC_PACKAGE.value,
@@ -15,16 +15,15 @@ val musicProviders = listOf(
         R.drawable.apple_music_white,
     ),
     MusicProvider(
-        "Spotify",
+        StringConstants.SPOTIFY_NAME.value,
         StringConstants.SPOTIFY.value,
-
         StringConstants.SPOTIFY_SEARCH.value,
         StringConstants.SPOTIFY_PACKAGE.value,
         false,
         R.drawable.spotify_white,
     ),
     MusicProvider(
-        "Anghami",
+        StringConstants.ANGHAMI_NAME.value,
         StringConstants.ANGHAMI.value,
         StringConstants.ANGHAMI_SEARCH.value,
         StringConstants.ANGHAMI_PACKAGE.value,
@@ -32,7 +31,7 @@ val musicProviders = listOf(
         R.drawable.anghami_white,
     ),
     MusicProvider(
-        "Youtube Music",
+        StringConstants.YT_MUSIC_NAME.value,
         StringConstants.YT_MUSIC.value,
         StringConstants.YT_MUSIC_SEARCH.value,
         StringConstants.YT_MUSIC_PACKAGE.value,
@@ -40,7 +39,7 @@ val musicProviders = listOf(
         R.drawable.yt_music_white,
     ),
     MusicProvider(
-        "Deezer", StringConstants.DEEZER.value,
+        StringConstants.DEEZER_NAME.value, StringConstants.DEEZER.value,
         StringConstants.DEEZER_SEARCH.value,
         StringConstants.DEEZER_PACKAGE.value,
         false,
--- Android/app/src/main/java/com/sal7one/musicswitcher/ui/ui/theme/Theme.kt
+++ Android/app/src/main/java/com/sal7one/musicswitcher/ui/ui/theme/Theme.kt
@@ -1,6 +1,7 @@
 package com.sal7one.musicswitcher.ui.ui.theme
 
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Shapes
 import androidx.compose.material.darkColors
 import androidx.compose.runtime.Composable
 
